983e0fc92c4b201064aa593df41034db36a9ea36
Thomas Ishak <tom.ishak.14@gmail.com>
2024-12-10 12:15:04 +1100

    My first commit, testing things

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('magicButton').addEventListener('click', () => {
+    const description = document.getElementById('description');
+    description.textContent = 'You clicked the button! 🎉';
+    description.style.color = '#4CAF50';
+});
